97905f0b6030aa3bb411b33bc27ec9ded07eac24
Malcom Fetus <sora.vista@gmail.com>
2020-08-26 21:13:31 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require 'pry'
+class CashRegister
 
+attr_accessor :total, :discount, :quantity
+
+  def initialize(discount = 0)
+  @total = 0 #CashRegister.new (from spec file) automatically sets total to 0 as starting point.
+  @discount = discount #CashRegister.new(20) (from sepc file) sets discount to 20.
+    end
+
+  def discount
+      @discount
+    end
+
+      def total
+        @total
+        end
+
+        def add_item(title, price)
+          @total += price * quantity
+          #binding.pry
+        end
+
+end
