b04e5e45f6622be31035ca2282e3800b9774d5ae
Malcom Fetus <sora.vista@gmail.com>
2020-08-26 21:21:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ attr_accessor :total, :discount, :quantity
 
   def initialize(discount = 0)
   @total = 0 #CashRegister.new (from spec file) automatically sets total to 0 as starting point.
-  @discount = discount #CashRegister.new(20) (from sepc file) sets discount to 20.
+  @discount = discount #CashRegister.new(20) (from spec file) sets discount to 20.
     end
 
   def discount
